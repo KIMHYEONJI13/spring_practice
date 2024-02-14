54f20da2ee9021ea268ddbb1be4b0376fab1a207
KIMHYEONJI13 <davithjk1310@gmail.com>
2024-02-14 15:53:09 +0900

    .

---
+++ chap01-ioc-container-practice/src/main/java/com/ohgiraffers/practice02/javaconfig/ContextConfiguration.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.practice02.javaconfig;
+
+import com.ohgiraffers.common.BoardDTO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("configurationPractice02")
+public class ContextConfiguration {
+
+    @Bean
+    public BoardDTO getBoard() {
+        return new BoardDTO(3L, "도서2", "책책책책", "북북북");
+    }
+
+}
